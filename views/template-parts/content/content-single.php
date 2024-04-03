c17c2c7f724c8e7fc44fe6c77625514384a4438b
Nishit Kumar <nishitkumar@Nishits-MacBook-Air-2.local>
2024-04-04 04:56:29 +0530

    style: Added css to improve the display layout of webinar single page.

--- views/template-parts/content/content-single.php
+++ views/template-parts/content/content-single.php
@@ -103,42 +103,66 @@ if ( ! function_exists( 'get_breadcrumb' ) ) {
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
-		<div class="st-program-info-container">
+		<div id="st-program-info-container" class="st-program-info-container">
 			<div class="st-day-container">
-				<span><?php esc_html_e( 'Day', 'st-webinar-management' ); ?></span>
-				<span>
-					<?php esc_html_e( 'Fourth-fair', 'st-webinar-management' ); ?>
-					<?php echo esc_attr( gmdate( 'd F', $start_date ) ); ?>
-				</span>
+				<div class="st-icon-container">
+					<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#21689D" class="bi bi-calendar" viewBox="0 0 16 16">
+						<path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5M1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4z"/>
+					</svg>
+				</div>
+				<div class="text-container">
+					<span><?php esc_html_e( 'Day', 'st-webinar-management' ); ?></span>
+					<span>
+						<?php esc_html_e( 'Fourth-fair', 'st-webinar-management' ); ?>
+						<?php echo esc_attr( gmdate( 'd F', $start_date ) ); ?>
+					</span>
+				</div>
 			</div>
 			<div  class="st-schedule-container">
-				<span><?php esc_html_e( 'Schedule', 'st-webinar-management' ); ?></span>
-				<span>
-					<?php echo esc_attr( gmdate( 'H, eO', $start_date ) ); ?>
-				</span>
+				<div class="st-icon-container">
+					<svg xmlns="http://www.w3.org/2000/svg" fill="#21689D" class="bi bi-clock" viewBox="0 0 16 16">
+						<path d="M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71z"/>
+						<path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16m7-8A7 7 0 1 1 1 8a7 7 0 0 1 14 0"/>
+					</svg>
+				</div>
+				<div class="text-container">
+					<span><?php esc_html_e( 'Schedule', 'st-webinar-management' ); ?></span>
+					<span>
+						<?php echo esc_attr( gmdate( 'H, eO', $start_date ) ); ?>
+					</span>
+				</div>
 			</div>
 			<div  class="st-duration-container">
-				<span><?php esc_html_e( 'Duration', 'st-webinar-management' ); ?></span>
-				<span>
-					<?php $start_date = strtotime( get_post_meta( get_the_ID(), 'duration', true ) ); ?>
-				</span>
+				<div class="st-icon-container">
+					<svg xmlns="http://www.w3.org/2000/svg" fill="#21689D" class="bi bi-hourglass-split" viewBox="0 0 16 16">
+						<path d="M2.5 15a.5.5 0 1 1 0-1h1v-1a4.5 4.5 0 0 1 2.557-4.06c.29-.139.443-.377.443-.59v-.7c0-.213-.154-.451-.443-.59A4.5 4.5 0 0 1 3.5 3V2h-1a.5.5 0 0 1 0-1h11a.5.5 0 0 1 0 1h-1v1a4.5 4.5 0 0 1-2.557 4.06c-.29.139-.443.377-.443.59v.7c0 .213.154.451.443.59A4.5 4.5 0 0 1 12.5 13v1h1a.5.5 0 0 1 0 1zm2-13v1c0 .537.12 1.045.337 1.5h6.326c.216-.455.337-.963.337-1.5V2zm3 6.35c0 .701-.478 1.236-1.011 1.492A3.5 3.5 0 0 0 4.5 13s.866-1.299 3-1.48zm1 0v3.17c2.134.181 3 1.48 3 1.48a3.5 3.5 0 0 0-1.989-3.158C8.978 9.586 8.5 9.052 8.5 8.351z"/>
+					</svg>
+				</div>
+				<div class="text-container">
+					<span><?php esc_html_e( 'Duration', 'st-webinar-management' ); ?></span>
+					<span>
+						<?php echo esc_html( get_post_meta( get_the_ID(), 'duration', true ) ); ?>
+					</span>
+				</div>
 			</div>
 		</div>
-		<figure class="post-thumbnail">
+		<figure id="st-post-thumbnail" class="st-post-thumbnail">
 			<img src="<?php echo esc_url_raw( get_the_post_thumbnail_url( get_the_ID() ) ); ?>" class="st-webinar-banner" />
 		</figure>
+		<div id="wp-block-st-webinar-management-webinar" class="wp-block-st-webinar-management-webinar">
 		<?php
-		the_content();
+		echo wp_kses_post( get_the_content() );
 
-		wp_link_pages(
-			array(
-				'before'   => '<nav class="page-links" aria-label="' . esc_attr__( 'Page', 'twentytwentyone' ) . '">',
-				'after'    => '</nav>',
-				/* translators: %: Page number. */
-				'pagelink' => esc_html__( 'Page %', 'twentytwentyone' ),
-			)
-		);
+		// wp_link_pages(
+		// 	array(
+		// 		'before'   => '<nav class="page-links" aria-label="' . esc_attr__( 'Page', 'twentytwentyone' ) . '">',
+		// 		'after'    => '</nav>',
+		// 		/* translators: %: Page number. */
+		// 		'pagelink' => esc_html__( 'Page %', 'twentytwentyone' ),
+		// 	)
+		// );
 		?>
+		</div>
 	</div><!-- .entry-content -->
 	<!-- Program Highlights -->
 	<div class="st-highlight-content">
